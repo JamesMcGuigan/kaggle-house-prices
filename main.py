992e054f9e94b3cbf7c3e39000169fce58aa8ba2
JamesMcGuigan <james.mcguigan.github@gmail.com>
2019-08-25 18:58:04 +0100

    LinearRegressionModel | write PolynomialFeatures.csv to file

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from src.models import LinearRegressionModel, FeatureEncoding
 pp = pprint.PrettyPrinter(depth=6)
 
 results = sorted([
-    FeatureEncoding().execute(),
-    LinearRegressionModel().execute()
+    LinearRegressionModel().execute(),
+    FeatureEncoding(features=['X_feature_exclude','X_feature_year_ages','X_feature_label_encode','X_feature_onehot']).execute(),
+    FeatureEncoding(features=['X_feature_exclude','X_feature_year_ages','X_feature_label_encode','X_feature_onehot','X_feature_polynomial'], outfile='./data/submissions/PolynomialFeatures.csv').execute(),
 ])
 for result in results:
     print( result )
